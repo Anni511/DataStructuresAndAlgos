3c3bacb909c1cd35b40c318d85dd11e8b44d20fe
Anni511 <aniruddhac7@gmail.com>
2018-03-16 12:31:04 +0530

    Updated Linear Search

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,12 +1,19 @@
+def LinearSearch(n, arr, ele):
+    flag = 0
+    for i in range(len(arr)):
+        if (arr[i] == ele):
+            return i+1
+    return -1
+
 n = int(input())
 arr = list(map(int,input().split()))
 ele = int(input())
-flag =0
-for i in range(len(arr)):
-    if (arr[i]==ele):
-        print(ele ," Found at :", i+1)
-        flag = 1
-        break
-
-if(flag==0):
-    print(ele, " Not Found")
+print("Element Found at : ", LinearSearch(n, arr,ele))
+# I/O Format
+# -1 means element was not found
+"""
+5
+4 5 3 6 7
+6
+Element Found at :  4
+"""
